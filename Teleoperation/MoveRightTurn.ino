c0715df5a8ebd88ad0eeb1a754aadb321ea59190
coleshanks <cshanks223@gmail.com>
2022-03-08 14:48:13 -0800

    Global file changed

--- Teleoperation/MoveRightTurn.ino
+++
@@ -1,8 +0,0 @@
-void MoveRightTurn() {
-
-      digitalWrite(L_F_DirectionPin, HIGH);
-      digitalWrite(R_F_DirectionPin, LOW);
-      delay(DelayTimeBackwardLeft);
-      analogWrite(L_F_motorPin, TurningSpeed);
-      analogWrite(R_F_motorPin, TurningSpeed00);
-}

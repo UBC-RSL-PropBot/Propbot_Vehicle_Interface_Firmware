e816e313f278d610dc1d7a27de979c39f75f5774
Apoorv <apoorvgarg17@gmail.com>
2022-03-08 13:52:14 -0800

    Sonar sensors added
    this is going to be the main file needed to integrate sonar sensors.

---
+++ SONAR_TEST_ALL_SENSORS/SONAR_TEST_ALL_SENSORS.ino
@@ -0,0 +1,133 @@
+
+#define SCL_PIN 0              //Default SDA is Pin5 PORTC for the UNO -- you can set this to any tristate pin
+#define SCL_PORT PORTD 
+#define SDA_PIN 1              //Default SCL is Pin4 PORTC for the UNO -- you can set this to any tristate pin
+#define SDA_PORT PORTD
+#define I2C_TIMEOUT 100        //Define a timeout of 100 ms -- do not wait for clock stretching longer than this time
+/*
+#define I2C_FASTMODE 1         //Run in fast mode (400 kHz)
+#define I2C_SLOWMODE 1         //If you do not define the mode it will run at 100kHz with this define set to 1 it will run at 25kHz
+*/
+#include <SoftI2CMaster.h>     //You will need to install this library
+
+void setup(){
+  // Initialize both the serial and I2C bus
+  Serial.begin(9600);
+  i2c_init();
+  pinMode(2,OUTPUT);
+
+  // (OPTIONAL) Check each address for a sensor
+  //address_polling_example();
+  //default_address_change_example();  
+  //change_address(224,218);
+      //Start the sensor and collect any error codes.
+  
+  // Your code here
+}
+
+void loop()
+{
+  // (OPTIONAL) Read a sensor at the default address
+  int range;
+  start_sensor(200);
+  delay(100);
+  range = read_sensor(200);   //reading the sensor will return an integer value -- if this value is 0 there was an error
+  Serial.print("R200:");Serial.println(range);
+  start_sensor(214);
+  delay(100);
+  range = read_sensor(214);   //reading the sensor will return an integer value -- if this value is 0 there was an error
+  Serial.print("R214:");Serial.println(range);
+  start_sensor(208);
+  delay(100);
+  range = read_sensor(208);   //reading the sensor will return an integer value -- if this value is 0 there was an error
+  Serial.print("R208:");Serial.println(range);
+  start_sensor(204);
+  delay(100);
+  range = read_sensor(204);   //reading the sensor will return an integer value -- if this value is 0 there was an error
+  Serial.print("R204:");Serial.println(range);  
+  start_sensor(212); 
+  delay(100);
+  range = read_sensor(212);   //reading the sensor will return an integer value -- if this value is 0 there was an error
+  Serial.print("R212:");Serial.println(range);
+  start_sensor(216);
+  delay(100);
+  range = read_sensor(216);   //reading the sensor will return an integer value -- if this value is 0 there was an error
+  Serial.print("R216:");Serial.println(range);
+  
+  // Your code here
+
+}
+
+
+
+///////////////////////////////////////////////////
+// Function: Start a range reading on the sensor //
+///////////////////////////////////////////////////
+boolean start_sensor(byte bit8address){
+  boolean errorlevel = 0;
+  bit8address = bit8address & B11111110;               //Do a bitwise 'and' operation to force the last bit to be zero -- we are writing to the address.
+  errorlevel = !i2c_start(bit8address) | errorlevel;   //Run i2c_start(address) while doing so, collect any errors where 1 = there was an error.
+  errorlevel = !i2c_write(81) | errorlevel;            //Send the 'take range reading' command. (notice how the library has error = 0 so I had to use "!" (not) to invert the error)
+  i2c_stop();
+  return errorlevel;
+}
+
+///////////////////////////////////////////////////////////////////////
+// Function: Read the range from the sensor at the specified address //
+///////////////////////////////////////////////////////////////////////
+//Uses the I2C library to read a sensor at the given address
+//Collects errors and reports an invalid range of "0" if there was a problem.
+//INPUTS: byte  bit8address = the address of the sensor to read from
+//OUPUTS: int   range = the distance in cm that the sensor reported; if "0" there was a communication error
+int read_sensor(byte bit8address){
+  boolean errorlevel = 0;
+  int range = 0;
+  byte range_highbyte = 0;
+  byte range_lowbyte = 0;
+  bit8address = bit8address | B00000001;  //Do a bitwise 'or' operation to force the last bit to be 'one' -- we are reading from the address.
+  errorlevel = !i2c_start(bit8address) | errorlevel;
+  range_highbyte = i2c_read(0);           //Read a byte and send an ACK (acknowledge)
+  range_lowbyte  = i2c_read(1);           //Read a byte and send a NACK to terminate the transmission
+  i2c_stop();
+  range = (range_highbyte * 256) + range_lowbyte;  //compile the range integer from the two bytes received.
+  if(errorlevel){
+    return 0;
+  }
+  else{
+    return range;
+  }
+}
+
+/////////////////////////////////////////
+// Function: Change the sensor address //
+/////////////////////////////////////////
+boolean change_address(byte oldaddress,byte newaddress){
+  //note that the new address will only work as an even number (odd numbers will round down)
+  boolean errorlevel = 0;
+  oldaddress = oldaddress & B11111110;  //Do a bitwise 'and' operation to force the last bit to be zero -- we are writing to the address.
+  errorlevel = !i2c_start(oldaddress) | errorlevel; //Start communication at the new address and track error codes
+  errorlevel = !i2c_write(170) | errorlevel;        //Send the unlock code and track the error codes
+  errorlevel = !i2c_write(165) | errorlevel;        //Send the unlock code and track the error codes
+  errorlevel = !i2c_write(newaddress) | errorlevel; //Send the new address
+  i2c_stop();
+  return errorlevel;
+}
+
+//////////////////////////////////////////////////////////
+// Read the sensor at the default address //
+//////////////////////////////////////////////////////////
+void read_the_sensor_example(){
+  boolean error = 0;  //Create a bit to check for catch errors as needed.
+  int range;  
+  //Take a range reading at the default address of 224
+  error = start_sensor(214);    //Start the sensor and collect any error codes.
+  if (!error){                  //If you had an error starting the sensor there is little point in reading it as you will get old data.
+    delay(100);
+    range = read_sensor(214);   //reading the sensor will return an integer value -- if this value is 0 there was an error
+    Serial.print("R:");Serial.println(range);
+    if (range >100){
+      digitalWrite(2,HIGH);
+      }
+     else {digitalWrite(2,LOW);}
+  }
+}
